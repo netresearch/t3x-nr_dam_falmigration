795d7239372cdc9a54fc8300437b0d03bafa982e
Christian Opitz <christian.opitz@netresearch.de>
2014-10-15 11:01:54 +0200

    AATD-8: Moved sql creation methods to abstract service

--- Classes/Service/AbstractService.php
+++ Classes/Service/AbstractService.php
@@ -147,6 +147,85 @@ abstract class AbstractService
         }
         return $storages;
     }
+
+    /**
+     * Create an insert query out of the given parameters - each of the strings can
+     * contain placeholders, which will be replaced by the given $vars
+     * 
+     * <example>
+     * $where = 'storage = :storageUid';
+     * $vars = array('storage' => 1);
+     * </example>
+     * 
+     * @param string $to    To table
+     * @param string $from  From table
+     * @param string $where WHERE condition
+     * @param string $order ORDER statement (including ASC/DESC)
+     * @param array  $vars  Variables to bind to the query (will be quoted into the
+     *                      query by replacing their keys prefix with a :)
+     * 
+     * @return void
+     */
+    protected function createInsertQuery(
+        $to, $from, $where, $order = null, array $vars = array()
+    ) {
+        if (!array_key_exists($to, $this->mapping)) {
+            throw new Exception\Error("No mapping for table {$to} found");
+        }
+            
+        $mapping = $this->mapping[$to];
+        $nt = PHP_EOL . '  ';
+        $sql = "INSERT INTO {$to} ({$nt}";
+        $sql .= implode(",{$nt}", array_keys($mapping));
+        $sql .= "\n) \nSELECT";
+        
+        foreach ($mapping as $toColumn => $fromColumn) {
+            $sql .= "{$nt} {$fromColumn} {$toColumn},";
+        }
+        
+        $sql = rtrim($sql, ',') . " \nFROM $from \nWHERE $where";
+        if ($order) {
+            $sql .= " \nORDER BY $order";
+        }
+        $sql .= ';';
+        
+        return $vars ? $this->quoteInto($vars, $sql) : $sql;
+    }
+    
+    /**
+     * Quote values into a statement
+     * 
+     * @param array  $values    Values, where key must be the name
+     * @param string $statement Statement string
+     * 
+     * @return string
+     */
+    protected function quoteInto(array $values, $statement)
+    {
+        uksort(
+            $values,
+            function ($a, $b) {
+                return strlen($a) < strlen($b);
+            }
+        );
+        
+        $search = array();
+        $replace = array();
+        foreach ($values as $key => $value) {
+            $search[] = ':' . $key;
+            if (is_bool($value)) {
+                $value = $value ? '1' : '0';
+            } elseif (is_numeric($value)) {
+                $value = is_int($value) ? (int) $value : (float) $value;
+            } elseif ($value === null) {
+                $value = 'NULL';
+            } else {
+                $value = $this->database->fullQuoteStr($value, '');
+            }
+            $replace[] = $value;
+        }
+        return str_replace($search, $replace, $statement);
+    }
     
     /**
      * Collect a query for later execution
