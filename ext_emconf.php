1d1657aff7122327c73efa3802e68802a609bdde
Christian Opitz <christian.opitz@netresearch.de>
2015-04-08 14:21:21 +0200

    TORG-141: Limiting maximum typo3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array(
     'shy' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '4.2.0-0.0.0',
+            'typo3' => '4.2.0-6.2.99',
         ),
         'conflicts' => array(
         ),
